f8c3faa05f103f489f1072bc6d3799ec2621df20
Jim deVos <jim.devos@gmail.com>
2023-11-13 09:55:08 -0700

    Pass Answering Test 2

---
+++ src/scenes/Answering/index.tsx
@@ -0,0 +1,18 @@
+//import React so that we can use JSX
+import React from "react";
+
+//import all the components from Semantic UI React
+import { Button, Container, Form, Header, TextArea } from "semantic-ui-react";
+
+const Answering = () => {
+  return (
+    <Container
+      data-testid="container"
+      style={{ position: "absolute", left: 200 }}
+    >
+      <Header data-testid="question" />;
+    </Container>
+  );
+};
+
+export default Answering;
