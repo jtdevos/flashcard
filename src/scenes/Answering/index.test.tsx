c32ad312c7ecb95403c7ef351219fffeea660ab3
Jim deVos <jim.devos@gmail.com>
2023-11-27 10:32:25 -0700

    finished section 4: CardContext
    - now onto section 5: Show the Answer and Submit Button
    - https://dev.to/jacobwicks/answer-display-and-submit-button-kja

--- src/scenes/Answering/index.test.tsx
+++ src/scenes/Answering/index.test.tsx
@@ -1,6 +1,5 @@
-import { CardState } from '../../types';
-import { CardProvider, initialState } from '../../services/CardContext';
-
+import { CardState } from "../../types";
+import { CardProvider, initialState } from "../../services/CardContext";
 
 //React lets us create and display components to the user
 //We need to import it so that we can look at the components to test them
@@ -9,7 +8,7 @@ import React from "react";
 //testing library gives us methods to test components
 //we use render to look at React components
 //we use cleanup to clear out memory after tests
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 //extend-expect gives us methods that let us say what we think a component will look like when we test it
 import "@testing-library/jest-dom/extend-expect";
@@ -18,12 +17,11 @@ import "@testing-library/jest-dom/extend-expect";
 //we have to import it so that we can look at it to test it
 import Answering from "./index";
 
-
 afterEach(cleanup);
 
 const renderAnswering = (testState?: CardState) => {
   return render(
-    <CardProvider testState={testState? testState : initialState}>
+    <CardProvider testState={testState ? testState : initialState}>
       <Answering />
     </CardProvider>
   );
@@ -46,23 +44,21 @@ it("has a Container", () => {
 });
 
 //test to see if the question prompt is in the document
-it('has the question prompt from the current card', () => {
-    const { cards, current } = initialState;
-    //get the question from current card 
-    const currentQuestion = cards[current].question;
+it("has the question prompt from the current card", () => {
+  const { cards, current } = initialState;
+  //get the question from current card
+  const currentQuestion = cards[current].question;
 
-    //get getByTestId from the helper function
-    const { getByTestId } = renderAnswering();
+  //get getByTestId from the helper function
+  const { getByTestId } = renderAnswering();
 
-    const question = getByTestId('question');
+  const question = getByTestId("question");
 
-    //question content should be the question from the current card
-    expect(question).toHaveTextContent(currentQuestion);
+  //question content should be the question from the current card
+  expect(question).toHaveTextContent(currentQuestion);
 });
 
 // todo: https://dev.to/jacobwicks/cardcontext-mhe#pass-answering-test-1-answering-shows-the-question-from-the-current-card
-
-
 
 it("has a button to skip the card", () => {
   //use Object Destructuring to get getByText from the result of render
@@ -103,4 +99,25 @@ it("Matches Snapshot", () => {
 
   //expect the result of asFragment() to match the snapshot of this component
   expect(asFragment()).toMatchSnapshot();
+});
+
+//test that the skip button works
+it("clicks the skip button and the next question appears", () => {
+  // create a CardState with current set to 0
+  const zeroState = {
+    ...initialState,
+    current: 0,
+  };
+
+  //current starts out at 0
+  const { getByTestId, getByText } = renderAnswering(zeroState);
+
+  const question = getByTestId("question");
+  //current starts out at 0, so question should be cards[0]
+  expect(question).toHaveTextContent(initialState.cards[0].question);
+
+  const skip = getByText(/skip/i);
+  fireEvent.click(skip);
+
+  expect(question).toHaveTextContent(initialState.cards[1].question);
 });
--- src/scenes/Answering/index.tsx
+++ src/scenes/Answering/index.tsx
@@ -1,17 +1,29 @@
 //import React so that we can use JSX
-import React from "react";
+import React, { useContext } from "react";
+//CardContext gives us access to the cards
+import { CardContext } from "../../services/CardContext";
+//The types of action that CardContext can handle
+import { CardActionTypes } from "../../types";
 
 //import all the components from Semantic UI React
 import { Button, Container, Form, Header, TextArea } from "semantic-ui-react";
 
 const Answering = () => {
+  //get cards and currrent index from CardContext
+  const { cards, current, dispatch } = useContext(CardContext);
+
+  //get the question from the current card
+  const { question } = cards[current];
+
   return (
     <Container
       data-testid="container"
       style={{ position: "absolute", left: 200 }}
     >
-      <Header data-testid="question" />
-      <Button>Skip</Button>
+      <Header data-testid="question" content={question} />
+      <Button onClick={() => dispatch({ type: CardActionTypes.next })}>
+        Skip
+      </Button>
       <Form>
         <TextArea data-testid="textarea" />
       </Form>
