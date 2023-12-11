f297b6e8564e6aed3f6b0c37427b1f3185619284
Jim deVos <jim.devos@gmail.com>
2023-12-11 15:51:10 -0700

    Done with 7: StatsContext; Onto 8: Show Stats
    - https://dev.to/jacobwicks/show-stats-3jnf

--- src/services/StatsContext/index.tsx
+++ src/services/StatsContext/index.tsx
@@ -1,4 +1,5 @@
 import { Stats, StatsAction, StatsState } from "../../types";
+import React, { createContext, useReducer } from "react";
 
 //the reducer handles actions
 export const reducer = (state: StatsState, action: StatsAction) => {
@@ -27,6 +28,52 @@ export const reducer = (state: StatsState, action: StatsAction) => {
 
       return newState;
     }
+
+    case "skip": {
+      //get the question from the card
+      const { question } = action;
+
+      //if tthe question is already in state, use those for the stats
+      //otherwise, use blankStats object
+      const prevStats = state[question] ? state[question] : blankStats;
+
+      //create newStats from the prevStats
+      const newStats = {
+        ...prevStats,
+        skip: prevStats.skip + 1,
+      };
+
+      //assign newStats to question
+      const newState = {
+        ...state,
+        [question]: newStats,
+      };
+
+      return newState;
+    }
+
+    case "wrong": {
+      const { question } = action;
+
+      //if the question is already in state, use those for the stats
+      //otherwise, use blankStats object
+      const prevStats = state[question] ? state[question] : blankStats;
+
+      //create newStats from the prevStats
+      const newStats = {
+        ...prevStats,
+        //wrong increases by 1
+        wrong: prevStats.wrong + 1,
+      };
+
+      //assign newStats to question
+      const newState = {
+        ...state,
+        [question]: newStats,
+      };
+      return newState;
+    }
+
     //default case returns the previous state with no modifications
     default:
       return state;
@@ -45,3 +92,27 @@ export const blankStats: Stats = {
 export const initialState = {
   dispatch: (action: StatsAction) => undefined,
 } as StatsState;
+
+const StatsContext = createContext(initialState);
+
+//the Props that the StatsProvider will accept
+type StatsProviderProps = {
+  //You can put react components inside of the Provider compnent
+  children: React.ReactNode;
+
+  //We might want to pass a state into SatsProvider for testing purposes
+  testState?: StatsState;
+};
+
+const StatsProvider = ({ children, testState }: StatsProviderProps) => {
+  const [state, dispatch] = useReducer(
+    reducer,
+    testState ? testState : initialState
+  );
+  const value = { ...state, dispatch } as StatsState;
+  return (
+    <StatsContext.Provider value={value}>{children}</StatsContext.Provider>
+  );
+};
+
+export { StatsContext, StatsProvider };
