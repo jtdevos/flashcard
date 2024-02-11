607f11647d4781ec48f23b0f37f93250bb168a0c
Jim deVos <jtdevos@asu.edu>
2024-02-11 01:01:14 -0700

    Save to localstorage: save tests 2-3
    Next up: https://dev.to/jacobwicks/saving-to-localstorage-18#add-saving-to-raw-cardcontext-endraw-

--- src/services/Save/index.test.ts
+++ src/services/Save/index.test.ts
@@ -1,36 +1,74 @@
-import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import { saveCards } from "./index";
-import { initialState } from "../CardContext";
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { loadCards, saveCards } from './index';
+import { initialState } from '../CardContext';
 
 afterEach(cleanup);
 
-describe("Saving and Loading Cards", () => {
-  let originalLocalStorage: Storage;
+//turn off console logging
+console.log = jest.fn();
 
-  beforeEach(() => {
-    originalLocalStorage = window.localStorage;
-  });
+describe('Saving and Loading Cards', () => {
+    let originalLocalStorage: Storage
 
-  afterEach(() => {
-    (window as any).loacalStorage = originalLocalStorage;
-  });
+	beforeEach(() => {
+		originalLocalStorage = window.localStorage
+	})
 
-  const { cards } = initialState;
-  const stringCards = JSON.stringify(cards);
+	afterEach(() => {
+        (window as any).localStorage = originalLocalStorage
+    })
+    
+    const { cards } = initialState;
+    const stringCards = JSON.stringify(cards);
+    
+    //saving cards saves cards
+    it('Saving cards saves cards', () => {
 
-  //saving cards saves cards
-  it("saving cards saves cards", () => {
-    const setItem = jest.spyOn(window.localStorage.__proto__, "setItem");
+        const setItem = jest.spyOn(window.localStorage.__proto__, 'setItem');
+        
+        saveCards(cards);
 
-    saveCards(cards);
+        expect(setItem).toHaveBeenCalledWith("cards", stringCards);
+    })
 
-    expect(setItem).toHaveBeenCalledWith("cards", stringCards);
-  });
-  //loading cards retrieves saved cards
-  //loading cards returns undefined if nothing found
+    //loading cards retrieves saved cards
+    it('Loading cards returns saved cards object', () => {
+        let mockGetItem = jest.fn().mockReturnValue(stringCards);
+        let localStorageMock = {
+            getItem: (params: any) => mockGetItem(params),
+        } 
+        Object.defineProperty(window, 'localStorage', {
+            value: localStorageMock,
+            writable: true
+            })
+            
+
+        const loadedCards = loadCards();
+        expect(mockGetItem.mock.calls.length).toBe(1);
+        expect(mockGetItem.mock.calls[0][0]).toBe('cards');
+        expect(loadedCards).toStrictEqual(cards);
+    });
+    
+    //loading cards returns undefined if nothing found
+    it('Loading cards when no saved cards returns undefined', () => {
+        let mockGetItem = jest.fn().mockReturnValue(undefined);
+        let localStorageMock = {
+            getItem: (params: any) => mockGetItem(params),
+        } 
+        Object.defineProperty(window, 'localStorage', {
+            value: localStorageMock,
+            writable: true
+            })
+            
+
+        const res = loadCards();
+        expect(mockGetItem.mock.calls.length).toBe(1);
+        expect(mockGetItem.mock.calls[0][0]).toBe('cards');
+        expect(res).toStrictEqual(undefined);
+    });
 });
 
 //saving stats saves stats
 //loading stats retrieves saved stats
-//loading stats returns epty object if nothing found
+//loading stats returns empty object if nothing found
