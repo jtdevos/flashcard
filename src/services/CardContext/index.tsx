4e2925bb921f3bbfc66493cec073f893f302051d
Jim deVos <jtdevos@asu.edu>
2024-02-17 13:10:10 -0700

    tests passing, next up: Save Test 3: Save stats

--- src/services/CardContext/index.tsx
+++ src/services/CardContext/index.tsx
@@ -1,32 +1,7 @@
 import React, { createContext, useEffect, useReducer } from "react";
 import { Card, CardState } from "../../types";
 import { saveCards } from "../Save";
-
-//declare a card object
-const card1: Card = {
-  question: "What is a linked list?",
-  subject: "Linked List",
-  //answer is inside of backticks
-  //this makes it a 'template literal`
-  //template literals can contain linebreaks
-  answer: `A linked list is a sequential list of nodes. 
-    The nodes hold data. 
-    The nodes hold pointers that point to other nodes containing data.`,
-};
-
-//declare another card object
-const card2: Card = {
-  question: "What is a stack?",
-  subject: "Stack",
-  answer: `A stack is a one ended linear data structure.
-    The stack models real world situations by having two primary operations: Push and pop.
-    Push adds an element to the stack.
-    Pop pulls the top element off of the stack.`,
-};
-
-//make an array with both cards
-//this is the starting deck of flashcards
-const cards = [card1, card2];
+import { getInitialState } from "./services";
 
 //the reducer handles actions
 export const reducer = (state: CardState, action: any) => {
@@ -34,6 +9,43 @@ export const reducer = (state: CardState, action: any) => {
   //if there is a case that matches the type it will run that code
   //otherwise it will run the default case
   switch (action.type) {
+    //delete a card
+    case "delete": {
+      let { cards, current } = state;
+      //the question is the unique identifier of a card
+      const { question } = action;
+
+      ///creating a new array of cards by spreading the current array of cards
+      const newCards = [...cards];
+
+      //finds the index of the target card
+      const index = newCards.findIndex((card) => card.question === question);
+
+      //splice removes the target card from the array
+      newCards.splice(index, 1);
+
+      //current tells the components what card to display
+      //decrement current
+      current = current - 1;
+
+      //don't pass -1 as current
+      if (current < 0) current = 0;
+
+      //spread the old state
+      //add the new value of current
+      //and return the newCards array as the value of cards
+      return {
+        ...state,
+        current,
+        cards: newCards,
+      };
+    }
+    case "new": {
+      return {
+        ...state,
+        current: -1,
+      };
+    }
     case "next": {
       //get cards and the current index from state
       const { cards, current } = state;
@@ -78,62 +90,13 @@ export const reducer = (state: CardState, action: any) => {
         cards: newCards,
       };
     }
-    case "new": {
-      return {
-        ...state,
-        current: -1,
-      };
-    }
-    case "delete": {
-      let { cards, current } = state;
-      //the question is the unique identifier of a card
-      const { question } = action;
-
-      ///creating a new array of cards by spreading the current array of cards
-      const newCards = [...cards];
-
-      //finds the index of the target card
-      const index = newCards.findIndex((card) => card.question === question);
-
-      //splice removes the target card from the array
-      newCards.splice(index, 1);
-
-      //current tells the components what card to display
-      //decrement current
-      current = current - 1;
-
-      //don't pass -1 as current
-      if (current < 0) current = 0;
-
-      //spread the old state
-      //add the new value of current
-      //and return the newCards array as the value of cards
-      return {
-        ...state,
-        current,
-        cards: newCards,
-      };
-    }
-
     //default case returns the previous state without changing it
     default:
       return state;
   }
 };
 
-//the object that we use to make the first Context
-//it is a cardState object
-export const initialState: CardState = {
-  //the deck of cards
-  cards,
-
-  //the index of the current card that components are looking at
-  current: 0,
-
-  //dispatch is a dummy method that will get overwritten with the real dispatch
-  //when we call useReducer
-  dispatch: ({ type }: { type: string }) => undefined,
-};
+export const initialState = getInitialState();
 
 //a context object made from initialState
 const CardContext = createContext(initialState);
@@ -145,9 +108,15 @@ type CardProviderProps = {
 
   //We might want to pass a state into the CardProvider for testing purposes
   testState?: CardState;
+
+  testDispatch?: (arg: any) => void;
 };
 
-const CardProvider = ({ children, testState }: CardProviderProps) => {
+const CardProvider = ({
+  children,
+  testState,
+  testDispatch,
+}: CardProviderProps) => {
   //useReducer returns an array containing the state at [0]
   //and the dispatch method at [1]
   //use array destructuring to get state and dispatch
@@ -157,6 +126,7 @@ const CardProvider = ({ children, testState }: CardProviderProps) => {
   );
 
   useEffect(() => {
+    //save cards to localStorage
     saveCards(state.cards);
   }, [state.cards]);
 
--- src/services/Save/index.test.ts
+++ src/services/Save/index.test.ts
@@ -1,72 +1,70 @@
-import { cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { loadCards, saveCards } from './index';
-import { initialState } from '../CardContext';
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { loadCards, saveCards } from "./index";
+import { initialState } from "../CardContext";
 
 afterEach(cleanup);
 
 //turn off console logging
-console.log = jest.fn();
+// console.log = jest.fn();
 
-describe('Saving and Loading Cards', () => {
-    let originalLocalStorage: Storage
+describe("Saving and Loading Cards", () => {
+  let originalLocalStorage: Storage;
 
-	beforeEach(() => {
-		originalLocalStorage = window.localStorage
-	})
+  beforeEach(() => {
+    console.log("window is", window.localStorage);
+    originalLocalStorage = window.localStorage;
+  });
 
-	afterEach(() => {
-        (window as any).localStorage = originalLocalStorage
-    })
-    
-    const { cards } = initialState;
-    const stringCards = JSON.stringify(cards);
-    
-    //saving cards saves cards
-    it('Saving cards saves cards', () => {
+  afterEach(() => {
+    // (window as any).localStorage = originalLocalStorage;
+  });
 
-        const setItem = jest.spyOn(window.localStorage.__proto__, 'setItem');
-        
-        saveCards(cards);
+  const { cards } = initialState;
+  const stringCards = JSON.stringify(cards);
 
-        expect(setItem).toHaveBeenCalledWith("cards", stringCards);
-    })
+  //saving cards saves cards
+  it("Saving cards saves cards", () => {
+    const setItem = jest.spyOn(window.localStorage.__proto__, "setItem");
 
-    //loading cards retrieves saved cards
-    it('Loading cards returns saved cards object', () => {
-        let mockGetItem = jest.fn().mockReturnValue(stringCards);
-        let localStorageMock = {
-            getItem: (params: any) => mockGetItem(params),
-        } 
-        Object.defineProperty(window, 'localStorage', {
-            value: localStorageMock,
-            writable: true
-            })
-            
+    saveCards(cards);
 
-        const loadedCards = loadCards();
-        expect(mockGetItem.mock.calls.length).toBe(1);
-        expect(mockGetItem.mock.calls[0][0]).toBe('cards');
-        expect(loadedCards).toStrictEqual(cards);
+    expect(setItem).toHaveBeenCalledWith("cards", stringCards);
+  });
+
+  //loading cards retrieves saved cards
+  it("Loading cards returns saved cards object", () => {
+    let mockGetItem = jest.fn().mockReturnValue(stringCards);
+    let localStorageMock = {
+      getItem: (params: any) => mockGetItem(params),
+    };
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+      writable: true,
     });
-    
-    //loading cards returns undefined if nothing found
-    it('Loading cards when no saved cards returns undefined', () => {
-        let mockGetItem = jest.fn().mockReturnValue(undefined);
-        let localStorageMock = {
-            getItem: (params: any) => mockGetItem(params),
-        } 
-        Object.defineProperty(window, 'localStorage', {
-            value: localStorageMock,
-            writable: true
-            })
-            
 
-        const res = loadCards();
-        expect(mockGetItem.mock.calls.length).toBe(1);
-        expect(mockGetItem.mock.calls[0][0]).toBe('cards');
-        expect(res).toStrictEqual(undefined);
+    const loadedCards = loadCards();
+    expect(mockGetItem.mock.calls.length).toBe(1);
+    expect(mockGetItem.mock.calls[0][0]).toBe("cards");
+    expect(loadedCards).toStrictEqual(cards);
+  });
+
+  //loading cards returns undefined if nothing found
+  it("Loading cards when no saved cards returns undefined", () => {
+    let mockGetItem = jest.fn().mockReturnValue(undefined);
+    let localStorageMock = {
+      getItem: (params: any) => mockGetItem(params),
+    };
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+      writable: true,
     });
+
+    const res = loadCards();
+    expect(mockGetItem.mock.calls.length).toBe(1);
+    expect(mockGetItem.mock.calls[0][0]).toBe("cards");
+    expect(res).toStrictEqual(undefined);
+  });
 });
 
 //saving stats saves stats
